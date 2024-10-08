8713e1b7c785b14ffab7af1dedbdad77e369a4b4
sunwchoi <chltjsdn103@naver.com>
2024-10-08 18:25:50 +0900

    Feat: 매치메이킹 로직 추가
    1. 매치메이킹 용 맵, 인스턴스, ui 추가

---
+++ Source/VRMilitarySimulation/Private/CSW/MissionLobbyWidget.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CSW/MissionLobbyWidget.h"
+
+#include "Components/Button.h"
+#include "Components/EditableText.h"
+#include "Components/ScrollBox.h"
+#include "Components/Slider.h"
+#include "Components/WidgetSwitcher.h"
+#include "CSW/SessionSlotWidget.h"
+#include "CSW/CSWGameInstance.h"
+
+void UMissionLobbyWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	auto* gi = Cast<UCSWGameInstance>(GetWorld()->GetGameInstance());
+	// OnSearchSignatureCompleteDelegate에 AddSessionSlotWidget을 연결하고싶다.
+	gi->OnSearchSignatureCompleteDelegate.AddDynamic(this , &UMissionLobbyWidget::AddSessionSlotWidget);
+
+	MENU_Button_GoCreateRoom->OnClicked.AddDynamic(this , &UMissionLobbyWidget::MENU_OnClickGoCreateRoom);
+	MENU_Button_GoFindSessions->OnClicked.AddDynamic(this , &UMissionLobbyWidget::MENU_OnClickGoFindSessions);
+
+	CR_Button_CreateRoom->OnClicked.AddDynamic(this , &UMissionLobbyWidget::CR_OnClickCreateRoom);
+	CR_Button_GoMenu->OnClicked.AddDynamic(this, &UMissionLobbyWidget::CR_OnClickGoMenu);
+
+	FR_Button_GoMenu->OnClicked.AddDynamic(this, &UMissionLobbyWidget::FR_OnClickGoMenu);
+
+}
+
+void UMissionLobbyWidget::MENU_OnClickGoCreateRoom()
+{
+	WidgetSwitcher->SetActiveWidgetIndex(1);
+}
+
+void UMissionLobbyWidget::MENU_OnClickGoFindSessions()
+{
+	WidgetSwitcher->SetActiveWidgetIndex(2);
+
+	auto* gi = Cast<UCSWGameInstance>(GetWorld()->GetGameInstance());
+	if ( gi )
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Click"));
+
+		gi->FindOtherSessions();
+	}
+}
+
+void UMissionLobbyWidget::CR_OnClickCreateRoom()
+{
+	auto* gi = Cast<UCSWGameInstance>(GetWorld()->GetGameInstance());
+	FString roomName = CR_Edit_RoomName->GetText().ToString();
+
+	// roomName이 기재되지 않거나 공백이라면 방생성을 하지 않고싶다.
+	roomName = roomName.TrimStartAndEnd();
+	if ( roomName.IsEmpty() )
+	{
+		return;
+	}
+
+	int32 count = (int32)CR_Slider_PlayerCount->GetValue();
+	gi->CreateMySession(roomName , count);
+}
+
+void UMissionLobbyWidget::CR_OnClickGoMenu()
+{
+	WidgetSwitcher->SetActiveWidgetIndex(0);
+}
+
+void UMissionLobbyWidget::FR_OnClickGoMenu()
+{
+	WidgetSwitcher->SetActiveWidgetIndex(0);
+
+}
+
+void UMissionLobbyWidget::AddSessionSlotWidget(const FRoomInfo& info)
+{
+	USessionSlotWidget* slot = CreateWidget<USessionSlotWidget>(this , SessionSlotWidgetFactory);
+	
+	slot->UpdateInfo(info);
+	// FS_ScrollBox에 추가 하고싶다.
+
+	FS_ScrollBox->AddChild(slot);
+}
--- Source/VRMilitarySimulation/VRMilitarySimulation.Build.cs
+++ Source/VRMilitarySimulation/VRMilitarySimulation.Build.cs
@@ -8,7 +8,7 @@ public class VRMilitarySimulation : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineSubsystemSteam" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineSubsystemSteam", "OnlineServicesOSSAdapter", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
@@ -16,7 +16,7 @@ public class VRMilitarySimulation : ModuleRules
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
