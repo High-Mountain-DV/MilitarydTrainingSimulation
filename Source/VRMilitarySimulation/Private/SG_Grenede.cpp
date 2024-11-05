adc1ab09f421945abef85103631dc9220c3bdea6
JSG0626 <tmdrjs1419@naver.com>
2024-11-05 13:43:34 +0900

    <Feat> 수류탄 클래스 생성, PlayMontage 노드 델리게이트 중복 바인딩 해결

---
+++ Source/VRMilitarySimulation/Private/SG_Grenede.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SG_Grenede.h"
+
+// Sets default values
+ASG_Grenede::ASG_Grenede()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASG_Grenede::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASG_Grenede::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
