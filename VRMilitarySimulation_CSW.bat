807acc4ddf367d7b991ad29c8db8b5b18ea43cca
sunwchoi <chltjsdn103@naver.com>
2024-11-13 15:45:18 +0900

    Feat: streaming 맵들로 연결

--- VRMilitarySimulation_CSW.bat
+++ VRMilitarySimulation_CSW.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Projects\Unreal\MilitarydTrainingSimulation\VRMilitarySimulation.uproject" VRLobbyMap -game -ResX=640 -RexY=480 -WinX=200 -WinY=300 -log -ReplaySupport
+"C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Projects\Unreal\MilitarydTrainingSimulation\VRMilitarySimulation.uproject" LobbyStreaming -game -ResX=640 -RexY=480 -WinX=200 -WinY=300 -log -ReplaySupport
