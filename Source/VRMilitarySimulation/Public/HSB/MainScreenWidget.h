04f960d5c847825200de95c740634a4776ea8f9e
Subin <80036437+so0oppy@users.noreply.github.com>
2024-12-18 22:18:56 +0900

    [Feat] 메인, 로딩 UI 구성

---
+++ Source/VRMilitarySimulation/Public/HSB/MainScreenWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainScreenWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRMILITARYSIMULATION_API UMainScreenWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
